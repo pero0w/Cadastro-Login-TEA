2be4d4fbe2a4d5e3e744a85b79555ce10be095af
pero0w <nathantsouza06@gmail.com>
2025-06-23 15:10:12 -0300

    a

--- backend/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'estudo1',
-  webDir: 'www/browser'
-};
-
-export default config;
